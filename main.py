b9b1d7f93138e1c703f99b88c8c1a7d122488a1d
chuotech <vincent.tran@hvmatl.org>
2026-01-28 02:46:14 -0500

    Create MIDI chord recognition

---
+++ main.py
@@ -0,0 +1,72 @@
+from music21 import converter, chord
+import pretty_midi
+import time
+import pprint
+
+class MIDI_Stream:
+    def __init__(self, midi_file, tempo = None):
+        self.midi_file = midi_file
+        self.tempo = tempo
+        self.midi_stream = pretty_midi.PrettyMIDI(midi_file)
+        self.duration = round(self.midi_stream.get_end_time())
+        if (tempo == None):
+            self.tempo = self.get_tempo()
+        self.notes = self.get_notes()
+        self.beats_total = int((self.tempo/60) * self.duration * 2)
+
+    def get_notes(self):
+        notes = []
+        for instrument in self.midi_stream.instruments:
+            for note in instrument.notes:
+                notes.append({"pitch": note.pitch, "onset": round(note.start, 5), "offset": round(note.end, 5)})
+        pprint.pp(notes)
+        return notes
+    
+    def get_tempo(self):
+        tempo_changes = self.midi_stream.get_tempo_changes()
+        if (len(tempo_changes[0])) > 0:
+            tempo = tempo_changes[1][0]
+            return tempo
+        else:
+            print("No tempo")
+            return
+        
+    def get_full_chord_list(self):
+        beats_total = int((self.tempo/60) * self.duration * 2)
+        quarter_length = 60 / self.tempo
+        eigth_length = quarter_length / 2
+        chord_list = []
+        curr_chord = []
+        # curr_highest = 0
+        # curr_lowest = 100000
+        curr_highest_pitch = 0
+        curr_lowest_pitch = 100000
+        curr_max_interval_length = eigth_length
+        i = 0
+        for interval in range(self.beats_total):
+            while i < len(self.notes) and (self.notes[i]["onset"] <= curr_max_interval_length):
+                curr_chord.append(self.notes[i]["pitch"])
+                if self.notes[i]["pitch"] >= curr_highest_pitch:
+                    # curr_highest = self.notes[i]["onset"]
+                    curr_highest_pitch = self.notes[i]["pitch"]
+                if self.notes[i]["pitch"] <= curr_lowest_pitch:
+                    # curr_lowest = self.notes[i]["onset"]
+                    curr_lowest_pitch = self.notes[i]["pitch"]
+                i += 1
+            chord_list.append((chord.Chord(curr_chord), str(interval + 1)))
+            curr_max_interval_length += eigth_length
+            # curr_highest = 0
+            # curr_lowest = 100000
+            if i < len(self.notes) and curr_max_interval_length >= self.notes[i]["onset"]:
+                curr_chord = []
+                curr_highest_pitch = 0
+                curr_lowest_pitch = 100000
+        pprint.pp(chord_list)
+        return chord_list
+        
+start = time.time()
+midi_path = ""
+midi_stream = MIDI_Stream(midi_path)
+chords = midi_stream.get_full_chord_list()
+end = time.time()
+print(end - start)
